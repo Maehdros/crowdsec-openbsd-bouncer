b3bc9597497edf10443bddfcf3c8979723d13705
Bruno Mairlot <bruno@maehdros.com>
2022-01-15 16:42:55 +0100

    Via stream

--- bouncer.php
+++ bouncer.php
@@ -6,10 +6,24 @@ require "vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+$options = getopt("",["startup"]);
+
 if(!isset($_ENV["APIKEY"])){
    die("Missing APIKEY in .env file");
 }
-$ch = curl_init($_ENV["LAPIURL"]."/v1/decisions");
+if(!isset($_ENV["LAPIURL"])){
+   die("Missing LAPIURL in .env file");
+}
+if(!isset($_ENV["PFTABLE"])){
+   die("Missing PFTABLE in .env file");
+}
+
+$startup="false";
+if(isset($options["startup"])){
+   $startup="true";
+}
+
+$ch = curl_init($_ENV["LAPIURL"]."/v1/decisions/stream?startup=$startup");
 $headers = array(
    "X-Api-Key: {$_ENV["APIKEY"]}",
    "Content-Type: application/json",
@@ -21,8 +35,17 @@ curl_setopt($ch,CURLOPT_SSL_VERIFYSTATUS,false);
 curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
 $response = curl_exec($ch);
 $arr = json_decode($response);
+
 $ipTxt="";
-foreach ($arr as $obj){
+foreach ($arr->deleted as $obj){
+   //$cmd = "/sbin/pfctl -t {$_ENV["PFTABLE"]} -T add $obj->value";
+   //exec($cmd,$output,$retval);
+   $ipTxt.=$obj->value.PHP_EOL;
+}
+file_put_contents("/tmp/ip.txt",$ipTxt);
+exec("pfctl -t crowdsec -T delete -f /tmp/ip.txt");
+$ipTxt="";
+foreach ($arr->new as $obj){
    //$cmd = "/sbin/pfctl -t {$_ENV["PFTABLE"]} -T add $obj->value";
    //exec($cmd,$output,$retval);
    $ipTxt.=$obj->value.PHP_EOL;
