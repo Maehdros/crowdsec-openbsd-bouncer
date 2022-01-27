33ebb36a6bebda2db3d3dc8fd608e6df5a182ec9
Bruno Mairlot <bruno@maehdros.com>
2022-01-27 10:56:08 +0100

    Debug .env

--- bouncer.php
+++ bouncer.php
@@ -36,6 +36,7 @@ curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
 curl_setopt($ch,CURLOPT_SSL_VERIFYSTATUS,false);
 curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
+curl_setopt($ch, CURLOPT_USERAGENT, "PHP CURL");
 $response = curl_exec($ch);
 $arr = json_decode($response);
 
